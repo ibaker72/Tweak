5dde6e1b88bd2ce20f6002d6c4fe364164160000
ibaker72 <Bakereyad7@gmail.com>
2026-04-24 14:05:45 +0000

    src/app/about/page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+
 import Link from "next/link";
 import { ArrowRight, Target, Gauge, Users, Code2 } from "lucide-react";
 import { Reveal } from "@/components/shared";
